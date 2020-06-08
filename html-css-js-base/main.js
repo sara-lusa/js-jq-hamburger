0a412f808e29564bf8ab6bfbb1315981907efc2a
Sara <saracorsob@gmail.com>
2020-06-08 15:39:38 +0200

    Aggiunta commenti

--- html-css-js-base/main.js
+++ html-css-js-base/main.js
@@ -1,9 +1,11 @@
+// Evento click per far apparire l'hamburger-menu
 $('.header-right > a').click(
   function() {
     $('.hamburger-menu').addClass('active');
   }
 );
 
+// Evento click per far scomparire l'hamburger menu
 $('a.close').click(
   function() {
     $('.hamburger-menu').removeClass('active');
